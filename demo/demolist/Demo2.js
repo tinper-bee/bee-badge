b29ee91eb52c4c96a77af238e6711995ba586135
Boyuzhou <2470200492@qq.com>
2018-01-21 21:59:59 +0800

    fix: 修改示例

--- demo/demolist/Demo2.js
+++ demo/demolist/Demo2.js
@@ -3,23 +3,23 @@
  * @description 在子元素里自定义内容
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Icon from 'bee-icon';
 import Badge from '../../src';
 
 class Demo2 extends Component {
-	render(){
-		return (
-			<div>
-			<Badge dataBadge={4} dataBadgePlacement="bottom">
-				<Icon type="uf-bell"></Icon>
-			</Badge>
-			<Badge dataBadge={4} colors="warning" dataBadgePlacement="up">
-				<Icon type="uf-bell"></Icon>
-			</Badge>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <Badge dataBadge={4} dataBadgePlacement="bottom">
+                    <Icon type="uf-bell"/>
+                </Badge>
+                <Badge dataBadge={4} colors="warning" dataBadgePlacement="up">
+                    <Icon type="uf-bell"/>
+                </Badge>
+            </div>
+        )
+    }
 }
 
 export default Demo2;
--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,8 @@
-
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
+import {Col, Row} from 'bee-layout';
+import {Panel} from 'bee-panel';
 import Button from 'bee-button';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Badge from '../src';
-import Icon from 'bee-icon';
 
 
 const CARET = <i className="uf uf-arrow-down"></i>;
@@ -13,38 +10,44 @@ const CARET = <i className="uf uf-arrow-down"></i>;
 const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
-var Demo1 = require("./demolist/Demo1");var Demo2 = require("./demolist/Demo2");var DemoArray = [{"example":<Demo1 />,"title":" 基本样式Badge","code":"/**\n * @title 基本样式Badge\n * @description `colors`控制背景颜色种类。\n */\n\nimport React, { Component } from 'react';\nimport Badge from 'bee-badge';\n\n class Demo1 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Badge colors=\"primary\" >8</Badge>\n\t\t)\n\t}\n}\n","desc":" `colors`控制背景颜色种类。"},{"example":<Demo2 />,"title":" 图标Badge","code":"/**\n * @title 图标Badge\n * @description 在子元素里自定义内容\n */\n\nimport React, { Component } from 'react';\nimport Icon from 'bee-icon';\nimport Badge from 'bee-badge';\n\nclass Demo2 extends Component {\n\trender(){\n\t\treturn (\n\t\t\t<div>\n\t\t\t<Badge dataBadge={4} dataBadgePlacement=\"bottom\">\n\t\t\t\t<Icon type=\"uf-bell\"></Icon>\n\t\t\t</Badge>\n\t\t\t<Badge dataBadge={4} colors=\"warning\" dataBadgePlacement=\"up\">\n\t\t\t\t<Icon type=\"uf-bell\"></Icon>\n\t\t\t</Badge>\n\t\t\t</div>\n\t\t)\n\t}\n}\n\n","desc":" 在子元素里自定义内容"}]
+var Demo1 = require("./demolist/Demo1");var Demo2 = require("./demolist/Demo2");var DemoArray = [{"example":<Demo1 />,"title":" 基本样式Badge","code":"/**\n * @title 基本样式Badge\n * @description `colors`控制背景颜色种类。\n */\n\nimport React, { Component } from 'react';\nimport { Badge } from 'tinper-bee';\n\n class Demo1 extends Component {\n\trender () {\n\t\treturn (\n\t\t\t<Badge colors=\"primary\" >8</Badge>\n\t\t)\n\t}\n}\n","desc":" `colors`控制背景颜色种类。"},{"example":<Demo2 />,"title":" 图标Badge","code":"/**\n * @title 图标Badge\n * @description 在子元素里自定义内容\n */\n\nimport React, {Component} from 'react';\nimport { Badge, Icon } from 'tinper-bee';\n\nclass Demo2 extends Component {\n    render() {\n        return (\n            <div>\n                <Badge dataBadge={4} dataBadgePlacement=\"bottom\">\n                    <Icon type=\"uf-bell\"/>\n                </Badge>\n                <Badge dataBadge={4} colors=\"warning\" dataBadgePlacement=\"up\">\n                    <Icon type=\"uf-bell\"/>\n                </Badge>\n            </div>\n        )\n    }\n}\n\n","desc":" 在子元素里自定义内容"}]
 
 
 class Demo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             open: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
+
     handleClick() {
-        this.setState({ open: !this.state.open })
+        this.setState({open: !this.state.open})
     }
 
-    render () {
-        const { title, example, code, desc  } = this.props;
+    render() {
+        const {title, example, code, desc, scss_code} = this.props;
         let caret = this.state.open ? CARETUP : CARET;
         let text = this.state.open ? "隐藏代码" : "查看代码";
 
-        const footer = (
-            <Button shape="block" onClick={ this.handleClick }>
-                { caret }
-                { text }
-            </Button>
+        const header = (
+            <div>
+                {example}
+                <Button style={{"marginTop": "10px"}} shape="block" onClick={this.handleClick}>
+                    {caret}
+                    {text}
+                </Button>
+            </div>
         );
         return (
             <Col md={12}>
-                <h3>{ title }</h3>
-                <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0,borderColor: "transparent"}} >
-                    <pre><code className="hljs javascript">{ code }</code></pre>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+                <Panel collapsible headerContent expanded={this.state.open} colors='bordered' header={header}
+                       footerStyle={{padding: 0}}>
+                    <pre><code className="hljs javascript">{code}</code></pre>
+                    {!!scss_code ? <pre><code className="hljs css">{scss_code}</code></pre> : null}
                 </Panel>
             </Col>
         )
@@ -52,20 +55,22 @@ class Demo extends Component {
 }
 
 class DemoGroup extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-    render () {
+
+    render() {
         return (
-                <Row>
-                    {DemoArray.map((child,index) => {
+            <Row>
+                {DemoArray.map((child, index) => {
 
-                        return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
-                        )
+                    return (
+                        <Demo example={child.example} title={child.title} code={child.code} scss_code={child.scss_code}
+                              desc={child.desc} key={index}/>
+                    )
 
-                    })}
-                </Row>
+                })}
+            </Row>
         )
     }
 }
